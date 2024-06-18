fb3fad373fb4b735e3e86e343c25db6c54aef61e
diniz12 <kelvynjdz@gmail.com>
2024-06-18 16:29:27 -0300

    Testng

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,17 +5,3 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const lastModified = document.lastModified;
     document.getElementById('lastModified').textContent = lastModified;
 });
-
-// Handle bfcache events
-window.addEventListener('pageshow', (event) => {
-    const perfEntries = performance.getEntriesByType("navigation");
-    if (perfEntries[0].type === 'back_forward') {
-        console.log('This page was loaded from bfcache.');
-    }
-});
-
-window.addEventListener('pagehide', (event) => {
-    if (event.persisted) {
-        console.log('This page is being put into bfcache.');
-    }
-});
