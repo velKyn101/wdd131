038d823a984dd6edf594c7936d837c2a18ebb259
diniz12 <kelvynjdz@gmail.com>
2024-06-18 16:52:17 -0300

    is it working?

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,21 +1,4 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    const currentYear = new Date().getFullYear();
-    document.getElementById('currentyear').textContent = currentYear;
-
-    const lastModified = document.lastModified;
-    document.getElementById('lastModified').textContent = lastModified;
-});
-
+window.addEventListener('DOMContentLoaded', (event) => {const currentYear = new Date().getFullYear();document.getElementById('currentyear').textContent = currentYear;const lastModified = document.lastModified;document.getElementById('lastModified').textContent = lastModified;});
 // Handle bfcache events
-window.addEventListener('pageshow', (event) => {
-    const perfEntries = performance.getEntriesByType("navigation");
-    if (perfEntries[0].type === 'back_forward') {
-        console.log('This page was loaded from bfcache.');
-    }
-});
-
-window.addEventListener('pagehide', (event) => {
-    if (event.persisted) {
-        console.log('This page is being put into bfcache.');
-    }
-});
+window.addEventListener('pageshow', (event) => {const perfEntries = performance.getEntriesByType("navigation");if (perfEntries[0].type === 'back_forward') {console.log('This page was loaded from bfcache.');}});
+window.addEventListener('pagehide', (event) => {if (event.persisted) {console.log('This page is being put into bfcache.');}});
