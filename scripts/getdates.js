5c4674fdb36d2801d2ccbe03609e1b139a5af64f
diniz12 <kelvynjdz@gmail.com>
2024-06-18 16:32:40 -0300

    one step back

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,3 +5,17 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const lastModified = document.lastModified;
     document.getElementById('lastModified').textContent = lastModified;
 });
+
+// Handle bfcache events
+window.addEventListener('pageshow', (event) => {
+    const perfEntries = performance.getEntriesByType("navigation");
+    if (perfEntries[0].type === 'back_forward') {
+        console.log('This page was loaded from bfcache.');
+    }
+});
+
+window.addEventListener('pagehide', (event) => {
+    if (event.persisted) {
+        console.log('This page is being put into bfcache.');
+    }
+});
