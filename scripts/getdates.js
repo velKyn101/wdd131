bfdf66e1eb0c853737361a8d6d3e9ee56297531d
diniz12 <kelvynjdz@gmail.com>
2024-07-15 18:47:37 -0300

    Site-Plan

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,5 @@
-window.addEventListener("DOMContentLoaded",()=>{const e=(new Date).getFullYear();document.getElementById("currentyear").textContent=e;const t=document.lastModified;document.getElementById("lastModified").textContent=t}),window.addEventListener("pageshow",e=>{"back_forward"===performance.getEntriesByType("navigation")[0].type&&console.log("This page was loaded from bfcache.")}),window.addEventListener("pagehide",e=>{e.persisted&&console.log("This page is being put into bfcache.")});
+window.addEventListener("DOMContentLoaded",()=>{
+    const e=(new Date).getFullYear();
+    document.getElementById("currentyear").textContent=e;
+    const t=document.lastModified;
+    document.getElementById("lastModified").textContent=t}),window.addEventListener("pageshow",e=>{"back_forward"===performance.getEntriesByType("navigation")[0].type&&console.log("This page was loaded from bfcache.")}),window.addEventListener("pagehide",e=>{e.persisted&&console.log("This page is being put into bfcache.")});
