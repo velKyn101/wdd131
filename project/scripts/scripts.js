69c602a2d33171ab9ccf07d729904219766d2753
diniz12 <kelvynjdz@gmail.com>
2024-07-20 22:15:58 -0300

    finishing the site

---
+++ project/scripts/scripts.js
@@ -0,0 +1,9 @@
+function myFunction(x) {
+  x.classList.toggle("change");
+  
+  const sidebar = document.getElementById('sidebar');
+  
+  sidebar.classList.toggle('show');
+  
+  document.body.classList.toggle('blur');
+}
